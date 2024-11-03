33962150f1412fe2948466f8feea1db61295482f
FilinMen <fiilinkadabra@tutamail.com>
2024-11-03 20:28:49 +0300

    0.5

--- plot.py
+++ plot.py
@@ -1,19 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math as m
 
-x = np.arange(-1, 1, 0.1)
+x = np.arange(-1, 1, 0.1) # значения которые может принимать х
 a = x ** 3
 b = x ** 2
-y = 40 * a - 3 * b - 20 * x + 5
+y = 40 * a - 3 * b - 20 * x + 5 # уравнение
 
-plt.title("График функции y=40х^3-3*x^2-20*x + 5", color='#000000', fontstyle="oblique", fontweight="bold")
-plt.xlabel('Ось х', color='#000000', fontweight="bold")
-plt.ylabel('Ось y', color='#000000', fontweight="bold")
+plt.title("График функции y=40х^3-3*x^2-20*x + 5", color='#000000', fontstyle="oblique", fontweight="bold")# оформление заглавления
+plt.xlabel('Ось х', color='#000000', fontweight="bold")# оформление надписи 
+plt.ylabel('Ось y', color='#000000', fontweight="bold")# оформление надписи 
 
-ax = plt.gca()
-ax.set_facecolor('#7B68EE')
-plt.gcf().set_facecolor('#7B68EE')
-
-plt.plot(x, y, color='#800000', marker='.', markersize=7)
+plt.plot(x, y, color='#800000', marker='.', markersize=7)# оформление графика 
 plt.show()
