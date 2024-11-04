f3f99093f876cedbab20df2c3847103c6ba3930f
FilinMen <fiilinkadabra@tutamail.com>
2024-11-04 18:58:56 +0300

    0.7

--- address_book.py
+++ address_book.py
@@ -3,27 +3,39 @@ my_dict = {
     "Iliy":"ул.Разбитых фонарей,д.16",
     'Bob': 'ул.Забвения,д.2'
 }
-print("Здравствуйте!!!")
-inp = input("изменить адресс жми 1, удаление записи жми 2, просмотр списка жми 0:")
-inp = int(inp)
-if inp == 1:
-    print(my_dict)
-    name = input("введите имя для изменения адреса:")
-    new_address = input("введите новый адрес:")
-    if name in my_dict: # проверка наналичие имени в списке
-        my_dict[name] = new_address # замена адреса если имя оказалось в списке
-        print(my_dict.items()) #вывод финального списка
+a = 1
+while a == 1:
+    print("ЗДРАВСТВУЙТЕ!!!")
+    inp = input("изменить адресс жми 1, удаление записи жми 2, просмотр списка жми 0:")
+    inp = int(inp)
+    if inp == 1:
+        print(my_dict)
+        name = input("введите имя для изменения адреса:")
+        new_address = input("введите новый адрес:")
+        if name in my_dict: # проверка наналичие имени в списке
+            my_dict[name] = new_address # замена адреса если имя оказалось в списке
+            print(my_dict.items()) #вывод финального списка
+        else:
+            print(f"имя {name} не найдено в адресной книге")
+        a = input("продолжить жми 1, завершить жми 2:")
+        a = int(a)
+    elif inp == 2:
+        if inp == 2:
+            print(my_dict)
+            name1 = input("введите имя которое хотите удалить:")
+            del my_dict[name1] # удаляем элемент из списка
+            print(my_dict.items()) #вывод финального списка
+            a = input("продолжить жми 1, завершить жми 2:")
+            a = int(a)
+        else:
+            print(f"имя {name} не найдено в адресной книге")
+            a = input("продолжить жми 1, завершить жми 2:")
+            a = int(a)
     else:
-        my_dict.setdefault(name, new_address) # если имени нет в списке, мы добовляем имя и адресс в конец списка
+        print("хорошо!")
         print(my_dict.items()) #вывод финального списка
-elif inp == 2:
-    print(my_dict)
-    name1 = input("введите имя которое хотите удалить:")
-    del my_dict[name1] # удаляем элемент из списка
-    print(my_dict.items()) #вывод финального списка
-else:
-    print("хорошо!")
-    print(my_dict.items()) #вывод финального списка
-    
+        a = input("продолжить жми 1, завершить жми 2:")
+        a = int(a)
+        
 
 
