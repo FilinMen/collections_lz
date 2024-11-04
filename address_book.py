396e9afa754d1d954aa2cd02caef555e182bc0f9
FilinMen <fiilinkadabra@tutamail.com>
2024-11-04 18:40:09 +0300

    0.6

--- address_book.py
+++ address_book.py
@@ -4,24 +4,26 @@ my_dict = {
     'Bob': 'ул.Забвения,д.2'
 }
 print("Здравствуйте!!!")
-inp = input("желаете изменить список жми 1, желаете просмотреть жми 0:")
+inp = input("изменить адресс жми 1, удаление записи жми 2, просмотр списка жми 0:")
 inp = int(inp)
 if inp == 1:
+    print(my_dict)
     name = input("введите имя для изменения адреса:")
     new_address = input("введите новый адрес:")
     if name in my_dict: # проверка наналичие имени в списке
         my_dict[name] = new_address # замена адреса если имя оказалось в списке
+        print(my_dict.items()) #вывод финального списка
     else:
         my_dict.setdefault(name, new_address) # если имени нет в списке, мы добовляем имя и адресс в конец списка
-    print(my_dict.items()) # выводим изменный список
-    print("желаете что-то удалить?")
-    inp1 = input("если да пиши 1, если нет пиши 0:")
-    inp1 = int(inp1)
-    if inp1 == 1:
-        name1 = input("введите имя которое хотите удалить:")
-        del my_dict[name1] # удаляем элемент из списка
-    else:
-        print("хорошо")
+        print(my_dict.items()) #вывод финального списка
+elif inp == 2:
+    print(my_dict)
+    name1 = input("введите имя которое хотите удалить:")
+    del my_dict[name1] # удаляем элемент из списка
+    print(my_dict.items()) #вывод финального списка
 else:
     print("хорошо!")
-print(my_dict.items()) #вывод финального списка
+    print(my_dict.items()) #вывод финального списка
+    
+
+
